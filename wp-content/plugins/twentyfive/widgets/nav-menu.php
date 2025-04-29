8b8b1a9516c183984e091dd2232526faf55607f2
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2025-04-29 20:37:33 +0600

    latest

--- wp-content/plugins/twentyfive/widgets/nav-menu.php
+++ wp-content/plugins/twentyfive/widgets/nav-menu.php
@@ -9,7 +9,7 @@ class Nav_Menu extends Widget_Base
 {
 
     private $display_menu_id = null;
-    
+
     public function __construct($data = [], $args = null)
     {
         parent::__construct($data, $args);
@@ -46,7 +46,7 @@ class Nav_Menu extends Widget_Base
 
         $menu_list = [];
 
-        foreach($menus as $menu) {
+        foreach ($menus as $menu) {
             $menu_list[$menu->slug] = $menu->name;
         }
 
@@ -60,6 +60,47 @@ class Nav_Menu extends Widget_Base
 
     public function register_controls()
     {
+
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__('Content Page', 'textdomain'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'pdf',
+            [
+                'label' => esc_html__('Choose PDF', 'twentyfive'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'media_types' => ['application/pdf'],
+            ]
+        );
+
+        $this->add_control(
+            'more_options',
+            [
+                'label' => esc_html__('Additional Options', 'twentyfive'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                'separator' => 'before',
+                'default' => 'Content goes here',
+            ]
+        );
+
+
+        $this->add_control(
+            'due_date',
+            [
+                'label' => esc_html__('Due Date', 'twentyfive'),
+                'type' => \Elementor\Controls_Manager::DATE_TIME,
+            ]
+        );
+
+
+        $this->end_controls_section();
+
+
         $this->start_controls_section(
             'menu_selection',
             [
@@ -69,10 +110,10 @@ class Nav_Menu extends Widget_Base
         );
 
         $this->add_control('menu', [
-            'label'       => __('Select Menu', 'twentyfive'),
-            'type'        => \Elementor\Controls_Manager::SELECT,
-            'options'     => $this->get_menus(),
-            'default'     => $this->get_default_slug(),
+            'label' => __('Select Menu', 'twentyfive'),
+            'type' => \Elementor\Controls_Manager::SELECT,
+            'options' => $this->get_menus(),
+            'default' => $this->get_default_slug(),
             'label_block' => true
         ]);
 
@@ -83,14 +124,14 @@ class Nav_Menu extends Widget_Base
             'menu_selection_style',
             [
                 'label' => __('Menu Style', 'twentyfive'),
-                'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
             ]
         );
-        
+
         /** bg color */
         $this->add_control('menu_bg_color', [
-            'label'     => __('Background Color', 'twentyfive'),
-            'type'      => \Elementor\Controls_Manager::COLOR,
+            'label' => __('Background Color', 'twentyfive'),
+            'type' => \Elementor\Controls_Manager::COLOR,
             'selectors' => [
                 '{{WRAPPER}} .twentyfive-menu' => 'background-color: {{VALUE}}'
             ]
@@ -98,18 +139,18 @@ class Nav_Menu extends Widget_Base
 
         /** link color */
         $this->add_control('menu_link_color', [
-            'label'     => __('Link Color', 'twentyfive'),
-            'type'      => \Elementor\Controls_Manager::COLOR,
+            'label' => __('Link Color', 'twentyfive'),
+            'type' => \Elementor\Controls_Manager::COLOR,
             'selectors' => [
                 '{{WRAPPER}} .twentyfive-menu a' => 'color: {{VALUE}}'
             ]
         ]);
 
-        
+
         /** link hover color */
         $this->add_control('menu_link_hover_color', [
-            'label'     => __('Link Hover Color', 'twentyfive'),
-            'type'      => \Elementor\Controls_Manager::COLOR,
+            'label' => __('Link Hover Color', 'twentyfive'),
+            'type' => \Elementor\Controls_Manager::COLOR,
             'selectors' => [
                 '{{WRAPPER}} .twentyfive-menu a:hover' => 'color: {{VALUE}}'
             ]
@@ -117,15 +158,15 @@ class Nav_Menu extends Widget_Base
 
         /** font family */
         $this->add_control('menu_text_font_family', [
-            'label'     => __('Font Family', 'twentyfive'),
-            'type'      => \Elementor\Controls_Manager::FONT,
-            'default'   => "'Open Sans', sans-serif",
+            'label' => __('Font Family', 'twentyfive'),
+            'type' => \Elementor\Controls_Manager::FONT,
+            'default' => "'Open Sans', sans-serif",
             'selectors' => [
                 '{{WRAPPER}} .twentyfive-menu a' => 'font-family: {{VALUE}}'
             ]
         ]);
 
-        
+
         $this->end_controls_section();
     }
 
@@ -144,19 +185,23 @@ class Nav_Menu extends Widget_Base
         $settings = $this->get_settings_for_display();
 
         $this->display_menu_id = $settings['menu'];
-        
+
+        echo "<div>" . $settings['more_options'] . "</div>";
         echo wp_nav_menu([
-            'menu'       => $this->display_menu_id,
-            'container'  => '',
+            'menu' => $this->display_menu_id,
+            'container' => '',
             'menu_class' => 'twentyfive-menu'
         ]);
     }
 
-    protected function _content_template()
+    protected function content_template()
     {
+        ?>
+        <div>{{{ settings.more_options }}}</div>
+        <?php
         echo wp_nav_menu([
-            'menu'       => $this->display_menu_id,
-            'container'  => '',
+            'menu' => $this->display_menu_id,
+            'container' => '',
             'menu_class' => 'twentyfive-menu'
         ]);
     }
